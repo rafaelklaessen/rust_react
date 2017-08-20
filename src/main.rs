07ad4dac4765cd0fb71dd0a0ebfc40a00d02386a
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-08-20 19:58:39 +0200

    Setup Iron

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate iron;
+extern crate router;
+extern crate staticfile;
+extern crate mount;
+
+use std::path::Path;
+
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+use staticfile::Static;
+use mount::Mount;
+
+fn handler(req: &mut Request) -> IronResult<Response> {
+    let ref query = req.extensions.get::<Router>()
+                        .unwrap()
+                        .find("query")
+                        .unwrap_or("/");
+    Ok(Response::with((status::Ok, *query)))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut router = Router::new();
+    router.get("/", handler, "index");
+    router.get("/:query", handler, "query");
+
+    let mut mount = Mount::new();
+    mount
+        .mount("/", router)
+        .mount("/static/eh", Static::new(Path::new("test/")));
+
+    Iron::new(mount).http("localhost:3000").unwrap();
 }
