3aa56e7cf1aef0dfb9225674008424b143683467
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-08-20 20:11:40 +0200

    Added index.html serving

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,25 @@ extern crate staticfile;
 extern crate mount;
 
 use std::path::Path;
+use std::fs::File;
+use std::io::Read;
 
 use iron::prelude::*;
 use iron::status;
+use iron::headers::ContentType;
+use iron::modifiers::Header;
 use router::Router;
 use staticfile::Static;
 use mount::Mount;
+
+#[allow(unused_variables)]
+fn index(req: &mut Request) -> IronResult<Response> {
+    let mut file = File::open("app/index.html").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    Ok(Response::with((status::Ok, Header(ContentType::html()), contents)))
+}
 
 fn handler(req: &mut Request) -> IronResult<Response> {
     let ref query = req.extensions.get::<Router>()
@@ -21,7 +34,7 @@ fn handler(req: &mut Request) -> IronResult<Response> {
 
 fn main() {
     let mut router = Router::new();
-    router.get("/", handler, "index");
+    router.get("/", index, "index");
     router.get("/:query", handler, "query");
 
     let mut mount = Mount::new();
